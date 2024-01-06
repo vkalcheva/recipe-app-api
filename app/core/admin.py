9bd974052aa40a1e26a38017924673b2a288a1af
Violeta Kalcheva <violetakalcheva@abv.bg>
2024-01-06 11:51:55 +0200

    Django admin customization and tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,16 @@
-# from django.contrib import admin
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
 
 
-def something():
-    pass
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
