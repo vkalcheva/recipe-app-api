542ab8ccbfbe60993b9a9cc4f7289999b35a554b
Violeta Kalcheva <violetakalcheva@abv.bg>
2024-04-04 12:43:46 +0300

    Changes

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -43,6 +43,6 @@ class AuthTokenSerializer(serializers.Serializer):
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authorization')
-        
+
         attrs['user'] = user
         return attrs
